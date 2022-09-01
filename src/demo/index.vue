9309676dd6455760294b5d9bbc74fdf31189b6f1
liuchang <liuchang@my.com>
2022-09-01 17:03:27 +0800

    Initial commit

---
+++ src/demo/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item , index) in arr" :key="index">
+        {{ item }}
+        <button @click="fn1(index)">删除</button>
+      </li>
+      <hr>
+    </ul>
+    <button @click="fn">点击生成随机数</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+    }
+
+  },
+  methods: {
+    fn() {
+      let num = Math.floor(Math.random() * 100)
+      this.arr.push(num)
+    },
+    fn1(idx) {
+      this.arr.splice(idx, 1)
+    }
+  },
+}
+</script>
